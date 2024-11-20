70d6be807d29d1bc6528efe2482d0a2757ebd2bd
Tay <taimarytrimino@gmail.com>
2024-11-20 10:33:08 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div class="promo">
+    <p>Envio gratis en compras de mas de $499</p>
+    <a href="">Acerca de</a>
+    <a href="">Contacto</a>
+    <a href="">Asistencia</a>
+    <a href="">Llama +52-1-33-12345678</a>
+  </div>
+
+  <RouterView />
+</template>
+
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import router from './router'
+
+
+import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).use(router).mount('#app')
+
