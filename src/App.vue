d3e09d710179e3838a8e4f86677a426eff1abfae
Tay <taimarytrimino@gmail.com>
2024-11-23 16:29:48 -0500

    toolbar configurations

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,31 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <v-responsive>
   <div class="promo">
-    <p>Envio gratis en compras de mas de $499</p>
+    <v-icon class="icon-box">mdi-package-variant-closed</v-icon>
+    <p class="envio">Envío gratis en compras de más de $499</p>
     <a href="">Acerca de</a>
     <a href="">Contacto</a>
     <a href="">Asistencia</a>
-    <a href="">Llama +52-1-33-12345678</a>
+    <a class="telefono" href="">Llama +52-1-33-12345678</a>
   </div>
-
+  <v-toolbar color="white" style="height: 100px;display: flex; justify-content: center; align-items: center;">
+    <v-toolbar-title style="font-size: 30px;">Perez Tec</v-toolbar-title>
+    
+    <div class="textfield" style="margin-right: 20px;"><v-text-field style="margin-top: 25px; margin-right: 80px;"
+            label="Buscar..."
+            variant="outlined"
+            width="290px"
+            append-inner-icon="mdi-magnify"
+            rounded
+          ></v-text-field></div>
+    <v-icon>mdi-account-circle</v-icon> <a class="entrar" href="">Entrar</a>
+    <v-icon style="margin-left: 20px;">mdi-heart-outline</v-icon> <a class="fav" href="">Favoritos</a>
+    <v-icon style="margin:0 20px;">mdi-cart-outline</v-icon>
+  </v-toolbar>
+  <div style="padding: 20px; background-color:#e5e5e5;"></div>
+  </v-responsive>
   <RouterView />
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import router from './router'
-
+import './/styles/style.css'
 
 import { createApp } from 'vue'
 
 // Vuetify
+import "@mdi/font/css/materialdesignicons.css"
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
